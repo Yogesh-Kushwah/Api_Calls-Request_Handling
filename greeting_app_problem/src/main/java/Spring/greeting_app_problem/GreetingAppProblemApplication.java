4e0d4df7d8bca284ffa80d7f8d40736ad940945a
unknown <yogeshkushwah775@gmail.com>
2025-02-24 16:16:42 +0530

    [Yogesh Kushwah] Add. GreetingController to use Services Layer to get Simple Greeting message Hello World

---
+++ greeting_app_problem/src/main/java/Spring/greeting_app_problem/GreetingAppProblemApplication.java
@@ -0,0 +1,13 @@
+package Spring.greeting_app_problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingAppProblemApplication.class, args);
+	}
+
+}
---
+++ greeting_app_problem/src/main/java/Spring/greeting_app_problem/controller/GreetingController.java
@@ -0,0 +1,51 @@
+package Spring.greeting_app_problem.controller;
+
+import Spring.greeting_app_problem.service.GreetingService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greet")
+public class GreetingController {
+
+
+    //UC01
+
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        return Map.of("message", "Hello from GET method!");
+    }
+
+    @PostMapping
+    public Map<String, String> postGreeting(@RequestBody Map<String, String> request) {
+        return Map.of("message", "Hello from POST method!", "received", request.get("name"));
+    }
+
+    @PutMapping
+    public Map<String, String> putGreeting(@RequestBody Map<String, String> request) {
+        return Map.of("message", "Hello from PUT method!", "updated", request.get("name"));
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        return Map.of("message", "Hello from DELETE method!");
+    }
+
+
+//UC02
+    //Extend GreetingController to use Services Layer to get Simple Greeting message ”Hello World”
+
+
+    private final GreetingService greetingService;
+
+    // Constructor-based Dependency Injection
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/q")
+    public Map<String, String> getGree() {
+        return Map.of("message", greetingService.getGreetingMessage());
+    }
+}
